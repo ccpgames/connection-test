0f4bcfa10e73194ac84d84eb325f3520cbfb2195
Gabríel Arthúr Pétursson <gabrielp@ccpgames.com>
2015-09-08 17:03:17 +0000

    Use log package

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -18,25 +19,34 @@ var versionNum = "1.0.4"
 var saveURLContents = flag.Bool("keepweb", false, "Stores the contents of http requests in individual files")
 
 func main() {
-	fmt.Printf("Running tests, results are being written to result.txt")
+	fmt.Println("Running tests, results are being written to result.txt")
 	flag.Parse()
 	path, err := os.Getwd()
+
 	if err != nil {
-		fmt.Printf("Fatal error: could not get working directory\r\n")
+		fmt.Println("Fatal error: could not get working directory")
 		panic(err)
 	}
+
 	outfile, err := os.Create(path + "/result.txt")
+
 	if err != nil {
-		fmt.Printf("Fatal error: could not create results file\r\n")
+		fmt.Println("Fatal error: could not create results file")
 		panic(err)
 	}
+
 	defer outfile.Close()
-	os.Stdout = outfile
-	fmt.Printf("CCP connection test tool version " + versionNum + "\r\n")
+
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	log.SetOutput(io.MultiWriter(os.Stdout, outfile))
+
+	log.Println("CCP connection test tool version ", versionNum)
+
 	if *saveURLContents == false {
-		fmt.Printf("successful web requests will not be stored for examination, specify -keepweb=true to store them\r\n")
+		log.Println("successful web requests will not be stored for examination, specify -keepweb=true to store them")
 	}
-	fmt.Printf("begin tests\r\n")
+
+	log.Printf("begin tests")
 	runTests()
 }
 
@@ -51,102 +61,122 @@ func runTests() {
 }
 
 func testPortOpen(port uint64) {
-	fmt.Printf("======PORT FORWARDING TEST======\r\n")
+	log.Println("======PORT FORWARDING TEST======")
+
 	urlStr := "http://tuq.in/tools/port.txt?port=" + strconv.FormatUint(port, 10)
 	resp, err := http.Get(urlStr)
+
 	if err != nil {
-		fmt.Printf("port open check failed, could not get address\r\n")
-		fmt.Println(err, "\r\n")
+		log.Println("port open check failed, could not get address")
+		log.Println(err)
 		return
 	}
+
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
-		fmt.Printf("port open check failed, could not read response\r\n")
-		fmt.Println(err, "\r\n")
+		log.Println("port open check failed, could not read response")
+		log.Println(err)
 		return
 	}
+
 	bodyString := string(body[:])
-	fmt.Printf("The scan of port " + strconv.FormatUint(port, 10) + " returned " + bodyString + "\r\n")
+	log.Println("The scan of port ", port, " returned ", bodyString)
 }
 
 func testLauncherURL(url string) {
-	fmt.Printf("======HTTP TEST======\r\n")
+	log.Println("======HTTP TEST======")
+
 	resp, err := http.Get(url)
+
 	if err != nil {
-		fmt.Printf("failed to connect to url " + url + " with error:\r\n")
-		fmt.Println(err, "\r\n")
+		log.Println("failed to connect to url " + url + " with error:")
+		log.Println(err)
 		return
 	}
+
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
-		fmt.Printf("Connected, but failed to read contents of URL " + url + " with error:\r\n")
-		fmt.Println(err, "\r\n")
+		log.Println("Connected, but failed to read contents of URL ", url, " with error:")
+		log.Println(err)
 		return
 	}
+
 	if *saveURLContents {
 		filename := "./" + cleanURL(url) + ".txt"
 		outfile, err := os.Create(filename)
+
 		if err != nil {
-			fmt.Printf("could not create results file\r\n")
-			panic(err)
+			log.Println("could not create results file")
+			log.Panicln(err)
 		}
+
 		defer outfile.Close()
 		bodyString := string(body[:])
 		outfile.WriteString(bodyString)
 	}
-	fmt.Printf("connected and read contents of " + url + " successfully\r\n")
 
+	log.Println("connected and read contents of ", url, " successfully")
 }
 
 func testPing() {
-	fmt.Printf("======PING TEST======\r\n")
+	log.Println("======PING TEST======")
+
 	if runtime.GOOS == "windows" {
 		cmd := exec.Command("ping", "87.237.38.200")
 		cmd.Stdout = os.Stdout
 		err := cmd.Run()
+
 		if err != nil {
-			fmt.Printf("OS does not have ping utility\r\n")
-			fmt.Println(err, "\r\n")
+			log.Println("OS does not have ping utility")
+			log.Println(err)
 			return
 		}
 	} else if runtime.GOOS == "darwin" {
 		cmd := exec.Command("ping", "-c 5", "87.237.38.200")
 		cmd.Stdout = os.Stdout
 		err := cmd.Run()
+
 		if err != nil {
-			fmt.Printf("OS does not have ping utility\r\n")
-			fmt.Println(err, "\r\n")
+			log.Println("OS does not have ping utility")
+			log.Println(err)
 			return
 		}
 	} else {
-		fmt.Printf("unsupported OS, no ping\r\n")
+		log.Println("unsupported OS, no ping")
 	}
 }
 
 func tcpConnect(port uint64) {
-	fmt.Printf("======TCP TEST======\r\n")
-	fmt.Printf("tcpConnect on port " + strconv.FormatUint(port, 10) + "\r\n")
+	log.Println("======TCP TEST======")
+
+	log.Println("tcpConnect on port ", port)
 	conStr := "87.237.38.200:" + strconv.FormatUint(port, 10)
 	conn, err := net.Dial("tcp", conStr)
+
 	if err != nil {
-		fmt.Printf("Error connecting:\r\n")
-		fmt.Println(err, "\r\n")
+		log.Println("Error connecting:")
+		log.Println(err)
 		return
 	}
+
 	defer conn.Close()
 	buffer := make([]byte, 0, 4096)
 	_, err = conn.Read(buffer)
+
 	if err != nil {
 		if err != io.EOF {
-			fmt.Printf("error reading buffer\r\n")
-			fmt.Println(err, "\r\n")
+			log.Println("error reading buffer")
+			log.Println(err)
 			return
 		}
 	}
-	fmt.Printf(string(buffer))
-	fmt.Printf("connection successful\r\n")
+
+	log.Println(string(buffer))
+	log.Println("connection successful")
 }
 
 func cleanURL(url string) string {
